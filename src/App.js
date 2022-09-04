a96305af8e638dffe86fa3d934fa53d34da1074b
Almir-Araujo <araujo.almir@gmail.com>
2022-09-04 12:23:09 -0300

    Inserção do H1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import StopWatch from './components/StopWatch/StopWatch';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <StopWatch />
     </div>
   );
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css'
+
+export default function Header() {
+  return (
+    <div>
+      <h1 className='header'>Cronômetro Online Gratuito</h1>
+    </div>  
+  );
+};
